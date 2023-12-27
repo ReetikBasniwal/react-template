e6e101e68bcd397079aaca0ae29dec2484561020
Reetik Basniwal <reetikbasniwal@gmail.com>
2023-12-27 17:25:11 +0530

    Steps from 2 to 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import './styles.css'
+import IMAGE from './logo.png';
+import LOGO from './react-2.svg';
+
 export const App = () => {
-    return <h1>React TS Webpack Starter Template</h1>
+    return <>
+        <h1>React TypeScript Webpack Starter Template</h1>
+        <img src={IMAGE} alt="React logo" width="300" height="300"/>
+        <img src={LOGO} alt="React logo" width="300" />
+    </>
 }
