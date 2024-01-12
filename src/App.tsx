f73305ce8c0501b686534435dcbff97f14910e6e
Reetik Basniwal <reetikbasniwal@gmail.com>
2024-01-12 17:06:06 +0530

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import LOGO from './react-2.svg'
 import ClickCounter from './ClickCounter'
 
 export const App = () => {
+  const name = 'Reetik'
   return (
     <>
       <h1>
-        Hello React TypeScript Webpack Starter Template - {process.env.NODE_ENV}{' '}
-        {process.env.name}
+        Hello React TypeScript Webpack Starter Template - {name}
+        {process.env.NODE_ENV} {process.env.name}
       </h1>
       <img src={IMAGE} alt="React logo" width="300" height="300" />
       <img src={LOGO} alt="React logo" width="300" />
